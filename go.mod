83f573c49b7e85d42dfa8db53fd8723242e84648
muhammadsiraf <derekzify@gmail.com>
2021-03-24 14:03:30 +0700

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/muhammadsiraf/module-test
+
+go 1.15
---
+++ tjetak.go
@@ -0,0 +1,19 @@
+package module_test
+
+import "fmt"
+
+func CetakHai() {
+	fmt.Println("Hai")
+}
+
+func CetakHello() {
+	fmt.Println("Hello")
+}
+
+func CetakSalam() {
+	fmt.Println("Salam")
+}
+
+func CetakBozz() {
+	fmt.Println("Bozz")
+}
